a5f9c400000a6d904bdbc32c8c0b167c4c9b1d6f
jopespfs <juaumpedro2@gmail.com>
2022-12-12 10:37:40 -0300

    mais sobre JS

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+console.log(2+3)
+console.log(2*3)
+
+let nome = "João Pedro Silveira"
+const dataNascimento = "30/07/1995"
+
+document.write("<h1>Bem Vindo</h1>")
+document.write(`<h2>Nascido em: ${dataNascimento}</h2>`)
+document.getElementsByTagName("h1")[0].style.color = "blue";
+document.write(nome)
+document.write("<br>")
+nome = "Lucas"
+document.write(nome)
+document.write("<br>")
+document.write(dataNascimento)
